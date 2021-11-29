1b81d92d28fd5eb9693abc84b8213a6b6b9f29b7
freestylejai1 <78399523+freestylejai1@users.noreply.github.com>
2021-11-29 12:45:38 -0800

    Add files via upload

---
+++ StocksWebScraper.js
@@ -0,0 +1,79 @@
+
+
+async function getHTML(url) {
+        var res = await fetch(url);
+        var html = await res.text();
+        return html;
+}
+function parseHtmlTarget(html, target) {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(html, "text/html");
+        //console.log(doc);
+        var targets = doc.querySelector(target);
+        var output = targets.innerHTML;
+        return output;
+
+}
+async function doWork() {
+        //Gets stock's ticker
+        var ticker = document.getElementById('searchTicker').value
+
+        //Gets stock quote
+        var quoteUrl = "https://www.cnbc.com/quotes/" + ticker
+        var quoteHtml = await getHTML(quoteUrl);
+        var quote = parseHtmlTarget(quoteHtml, '.QuoteStrip-lastPrice');
+
+        //Gets stock description
+        var descUrl = "https://www.cnbc.com/quotes/" + ticker
+        var descHtml = await getHTML(descUrl);
+        var desc = parseHtmlTarget(descHtml, '.CompanyProfile-summary');
+
+        //Writes data
+        var textnode1 = document.createTextNode(quote);
+        var valnode1 = document.getElementById("currentValue");
+        while (valnode1.firstChild)
+          valnode1.removeChild(valnode1.firstChild);
+        valnode1.appendChild(textnode1);
+
+        var textnode2 = document.createTextNode(desc);
+        var valnode2 = document.getElementById("stockDescription");
+        while (valnode2.firstChild)
+          valnode2.removeChild(valnode2.firstChild);
+        valnode2.appendChild(textnode2);
+}
+
+/*
+async function getHTML(url) {
+  var res = await fetch(url);
+  var html = await res.text();
+  return html;
+}
+function parseHtmlForQuote(html, target) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(html, "text/html");
+  var targets = doc.querySelector(target);
+  var output = targets.innerHTML;
+  return output;
+}
+async function doWork() {
+  //Gets stock's ticker
+  var ticker = document.getElementById('searchTicker').value
+
+  //Gets stock quote
+  var quoteUrl = "https://www.cnbc.com/quotes/" + ticker
+  var quoteHtml = await getHTML(quoteUrl);
+  var quote = parseHtmlForQuote(quoteHtml, '.QuoteStrip-lastPrice');
+
+  //Gets stock description
+  var descUrl = "https://www.cnbc.com/quotes/" + ticker
+  var descHtml = await getHTML(descUrl);
+  var desc = parseHtmlTarget(descHtml, '.CompanyProfile-summary');
+
+  var textnode = document.createTextNode(quote);
+  var valnode = document.getElementById("currentValue");
+  while (valnode.firstChild)
+    valnode.removeChild(valnode.firstChild);
+  valnode.appendChild(textnode);
+
+}
+*/
